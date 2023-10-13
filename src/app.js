d4e84493a11cabf9b448bdadcdb48e1efdf94f0c
Miguel5040 <miguelssj5040@gmail.com>
2023-10-13 01:33:49 +0000

    Entrega de proyecto

--- src/app.js
+++ src/app.js
@@ -7,5 +7,21 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  let pronombre = ["el", "nuestro"];
+  let adj = ["gran", "pequeño"];
+  let sustantivo = ["tigre", "gato"];
+  let extension = [".com", ".net", ".io", ".es"];
+
+  for (let i = 0; i < pronombre.length; i++) {
+    for (let e = 0; e < adj.length; e++) {
+      for (let a = 0; a < sustantivo.length; a++) {
+        for (let u = 0; u < extension.length; u++) {
+          const dominioFinal =
+            pronombre[i] + adj[e] + sustantivo[a] + extension[u];
+          console.log(dominioFinal);
+        }
+      }
+    }
+  }
 };
